66db9495859066fcf63eac187c4d0ee998d9a714
perttrus97 <155573486+perttrus97@users.noreply.github.com>
2025-05-24 03:04:38 +0200

    Update init_holders.js

--- scripts/init_holders.js
+++ scripts/init_holders.js
@@ -1,6 +1,6 @@
 const init_holders = [
   {
-    // private key is 0x9b28f36fbd67381120752d6172ecdcf10e06ab2d9a1367aac00cdcd6ac7855d3, only use in dev
+    // private key is 0x5F38029B6a3416b7a866EEFa15cf080Ee80c9F48, only use in dev
     address: '0x9fB29AAc15b9A4B7F17c3385939b007540f4d791',
     balance: BigInt('10000000000000000000000000').toString(16),
   },
